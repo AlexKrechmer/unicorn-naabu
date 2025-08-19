93038e05f18e9be148e6c3ae1d30dfd677e18463
Alex Krechmer <Alex.Krechmer@Gmail.com>
2025-08-19 17:23:56 -0400

    Add unicorn_scan custom Go scanner with Nmap integration

---
+++ unicorn_scan.go
@@ -0,0 +1,143 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+	"os/exec"
+	"strings"
+	"time"
+)
+
+// ANSI color codes
+const (
+	Purple = "\033[35m"
+	Cyan   = "\033[36m"
+	Green  = "\033[32m"
+	Yellow = "\033[33m"
+	Reset  = "\033[0m"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: ./scan <target>")
+		return
+	}
+
+	target := os.Args[1]
+
+	// Resolve hostname to IP
+	ips, err := net.LookupIP(target)
+	if err != nil {
+		fmt.Printf("Could not resolve target %s: %v\n", target, err)
+		return
+	}
+	targetIP := ips[0].String()
+	fmt.Printf("Scanning IP: %s\n", targetIP)
+
+	// Unicorn ASCII art
+	unicornArt := `
+⠀⠀⠀⠀⠀⠑⢦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠙⢷⣦⣀⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢿⣷⣿⣾⣿⣧⣄⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⣿⣿⣿⣿⣿⣿⣿⣇⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣥⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠸⣿⠟⠉⠉⢹⣿⣿⣿⣿⣿⣿⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣿⣿⣿⣿⣿⣿⣿⣿⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⢀⣠⣶⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡄⠀⠀⠀⠀⠀⠀⠀⠀
+⢀⣴⠿⠛⠉⢸⡏⠁⠉⠙⠛⠻⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣄⡀⠀⠀⠀⠀⠀
+⠉⠉⠀⠀⠀⢸⡇⠀⠀⠀⠀⠀⠀⠙⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⡀⠀⠀⠀
+⠀⠀⠀⠀⠀⠈⠿⠀⠀⠀⠀⠀⠀⠀⠀⠙⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠻⢿⣿⣿⣿⣿⣿⣿⣧⡀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣿⣿⣿⣿⣿⠟⢿⣷⡄
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⣿⡟⠀⢠⣾⣿⣿
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⣿⣿⣀⣾⣿⡿⠃
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⣿⠏⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣿⣿⠻⣿⣿⡀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠐⠋⣹⣿⠃⠀⠈⣿⣿⣴⠇
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⣾⠟⠀⠀⠀⠀⠘⠉⠛⠀
+`
+
+	// Naabu banner ASCII
+	naabuBanner := `
+  ___  ___  ___ _/ /  __ __
+ / _ \/ _ \/ _ \/ _ \/ // /
+/_//_/\_,_/\_,_/_.__/\_,_/
+
+                projectdiscovery.io
+`
+	// Print unicorn and banner
+	fmt.Println(Purple + unicornArt + Reset)
+	fmt.Println(Purple + naabuBanner + Reset)
+	fmt.Printf("%s[*] Scanning target: %s%s\n\n", Purple, target, Reset)
+
+	// Spinner goroutine
+	spinnerDone := make(chan bool)
+	go func() {
+		spin := []rune{'|', '/', '-', '\\'}
+		i := 0
+		for {
+			select {
+			case <-spinnerDone:
+				return
+			default:
+				fmt.Printf("\r%sScanning %s... %c%s", Yellow, target, spin[i%len(spin)], Reset)
+				i++
+				time.Sleep(100 * time.Millisecond)
+			}
+		}
+	}()
+
+	// Run Naabu
+	naabuCmd := exec.Command("naabu", "-silent", "-host", targetIP)
+	stdout, err := naabuCmd.StdoutPipe()
+	if err != nil {
+		fmt.Println("Error capturing Naabu output:", err)
+		return
+	}
+	naabuCmd.Stderr = os.Stderr
+	if err := naabuCmd.Start(); err != nil {
+		fmt.Println("Error starting Naabu:", err)
+		return
+	}
+
+	ports := []string{}
+	scanner := bufio.NewScanner(stdout)
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		if line != "" {
+			parts := strings.Split(line, ":")
+			if len(parts) > 1 {
+				ports = append(ports, parts[1])
+			} else {
+				ports = append(ports, parts[0])
+			}
+		}
+	}
+	naabuCmd.Wait()
+	spinnerDone <- true
+	fmt.Println("\rScan complete.                 ")
+
+	if len(ports) == 0 {
+		fmt.Println("No open ports found by Naabu. Defaulting to 80,443 for Nmap scan.")
+		ports = []string{"80", "443"}
+	}
+
+	portList := strings.Join(ports, ",")
+	fmt.Printf("%s[Naabu] Open ports: %s%s\n\n", Purple, portList, Reset)
+
+	// Run Nmap
+	fmt.Println(Cyan + "[*] Running Nmap..." + Reset)
+	nmapCmd := exec.Command("nmap", "-sC", "-sV", "-p", portList, target)
+	nmapOut, err := nmapCmd.CombinedOutput()
+	if err != nil {
+		fmt.Println("Error running Nmap:", err)
+		return
+	}
+	fmt.Printf("%s%s%s\n", Cyan, string(nmapOut), Reset)
+
+	fmt.Println(Green + "[+] Scan summary complete." + Reset)
+}
